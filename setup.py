3a6a6b9f079c0c71afc45708547bc81d0a488e7c
UzCoder123 <salohiddinsalohiddin345@gmail.com>
2024-08-19 14:59:20 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='uzbtelebot',
-    version='0.0.8',
+    name='library name',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        'telebot',  # telebot kutubxonasini o'rnatish uchun
+        '',
     ],
-    author='SalohiddinEsanbekov',
-    author_email='salohiddinsalohiddin123@gmail.com',
-    description='Agar siz telegram bot yasashni beginner darajasida bo\'lsangiz bu kutubxona siz uchun siz bu kutubxona orqali telegram bot yarata olasiz\n\nKutubxonani o\'rnatishpip install uzbtelebot',
+    author='author name',
+    author_email='yourgamil@gmail.com',
+    description='library descreption',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
